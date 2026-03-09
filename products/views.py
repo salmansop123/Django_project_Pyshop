1dc876144c036043316e0dd22d5677c1f538ce13
Salman Mazhar <salmansop123@gmail.com>
2026-03-09 23:08:17 +0500

    Route concept is cleared and practice it

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+
+def index(request):
+    return HttpResponse("Hello World!")
+
+
+def new(request):
+    return HttpResponse("New Product")
+
+
+def addProduct(request):
+    return HttpResponse("Your product is added")
