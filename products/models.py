88681f179f54f1c17ee68c2222cd11d0de42d10c
Salman Mazhar <salmansop123@gmail.com>
2026-03-09 23:50:42 +0500

    Databse is connected to the project and make the migrations to the DB lite as well

--- products/models.py
+++ products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.CharField()
+    stock = models.CharField()
+    image_url = models.CharField(max_length=2083) #This is industry standard for url
+
+
+class Offer(models.Model):
+    code = models.CharField(max_length=10)
+    description = models.CharField("Product Discount 20 percent", max_length=255)
+    discount = models.FloatField()
--- pyshop/urls.py
+++ pyshop/urls.py
@@ -22,4 +22,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("products/", include("products.urls")) #This will diert the client to the correct url that we define "we define this oonly once to tell where to takr the client"
+
 ]
