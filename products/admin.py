993491c391dc99b6712e37b586112b965ed8cf95
Salman Mazhar <salmansop123@gmail.com>
2026-03-10 22:49:10 +0500

    jango dynamic remder in html markup

--- products/admin.py
+++ products/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Offer
 
-admin.site.register(Product)
+class ProductAdmin (admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+
+class ProductOffer (admin.ModelAdmin):
+    list_display = ("code", "discount")
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Offer, ProductOffer)
